77210ee0c044f477b79b056641fbeb2c1b3cc3d1
hanan tino <rinr9430@gmail.com>
2024-07-09 16:59:39 +0700

    test

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Alec Thompson',
+            'email' => 'admin@corporateui.com',
+            'password' => Hash::make('secret'),
+            'about' => "Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality).",
+        ]);
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+            'about' => "Ini admin",
+        ]);
+    }
+}
